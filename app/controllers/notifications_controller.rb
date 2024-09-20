5afa28c90b432f99ecdcc0f55287c6a06bad6ce3
Jin Sofue <jin01213921@gmail.com>
2024-09-20 16:37:55 +0900

    kaminariに移行

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@
 
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.paginate(page: params[:page])
+    @notifications = current_user.passive_notifications.page(params[:page])
     @notifications.where(checked: false).find_each do |notification|
       notification.update_attribute(:checked, true)
     end
