1f6c26837b740abc72471c51e8ef7be2495f8d5f
Jin Sofue <jin01213921@gmail.com>
2024-09-21 10:52:34 +0900

    リポストしたマイクロポストがフォームのフィードに流れるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -110,7 +110,9 @@ class User < ApplicationRecord
   def feed
     following_ids = "SELECT followed_id FROM relationships
                     WHERE follower_id = :user_id"
-    Micropost.where("user_id IN (#{following_ids})
+    following_user_reposted_micropost_ids      = "SELECT micropost_id FROM reposts
+                                                  WHERE user_id IN (#{following_ids}) "
+    Micropost.where("id IN (#{following_user_reposted_micropost_ids}) OR user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
   end
 
